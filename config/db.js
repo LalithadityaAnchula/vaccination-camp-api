46e02ea62b79d12b429bd3d8871a1e04b8a82b30
LalithadityaAnchula <lalithaditya903@gmail.com>
2022-07-12 00:50:56 +0530

    remove unwanted code from db.js

--- config/db.js
+++ config/db.js
@@ -5,12 +5,6 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {});
   console.log(`MongoDB Connected: ${conn.connection.host}`);
   const db = await mongoose.connection.db;
-  // db.collection("users")
-  //   .find({})
-  //   .toArray(function (err, result) {
-  //     if (err) throw err;
-  //     console.log(result);
-  //   });
 };
 
 module.exports = connectDB;
